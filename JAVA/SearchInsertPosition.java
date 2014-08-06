3248beabb547be160da1d36f1c7563da4184a264
sezina <sezinamd@gmail.com>
2014-08-06 13:27:57 +0800

    apdt: SearchInsertPosition.java

--- JAVA/SearchInsertPosition.java
+++ JAVA/SearchInsertPosition.java
@@ -1,25 +1,18 @@
 public class Solution {
     public int searchInsert(int[] A, int target) {
-        int pos = -1;
         int size = A.length;
         int low = 0, high = size - 1;
-        while (low < high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             if (target < A[mid]) {
-                high = mid;
+                high = mid - 1;
             } else if (target > A[mid]) {
                 low = mid + 1;
             } else {
-                pos = mid;
-                break;
+                return mid;
             }
         }
-        if (pos < 0) {
-            if (target <= A[low])
-                pos = low;
-            else if (target > A[low])
-                pos = low + 1;
-        }
-        return pos;
+        
+        return low;
     }
 }
