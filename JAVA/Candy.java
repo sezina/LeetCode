f7a7e6a4be75ea7018ac29dcc236805cd724e39b
sezina <sezinamd@gmail.com>
2014-08-16 13:27:28 +0800

    updt: Candy.java

--- JAVA/Candy.java
+++ JAVA/Candy.java
@@ -28,3 +28,19 @@ public class Solution {
         return result;
     }
 }
+
+public class Solution2 {
+    public int candy(int[] ratings) {
+        int[] candy = new int[ ratings.length ];
+        candy[0] = 1;
+        for(int i = 1; i < ratings.length; i++){
+            candy[i] = ratings[i] > ratings[i-1] ? candy[i-1]+1 : 1;
+        }
+        int totalCandy = candy[ratings.length-1];
+        for(int i = ratings.length-2; i >= 0; i--){
+            candy[i] = (ratings[i] > ratings[i+1] && candy[i+1]+1 > candy[i]) ? candy[i+1]+1 : candy[i];
+            totalCandy += candy[i];
+        }
+        return totalCandy;
+    }
+}
