4676daf1868cfcab4593dd1de7d0114bcd12d940
sezina <sezinamd@gmail.com>
2014-08-20 09:44:05 +0800

    updt: ReverseWordsInAString.java

--- JAVA/ReverseWordsInAString.java
+++ JAVA/ReverseWordsInAString.java
@@ -1,3 +1,4 @@
+// using split method
 public class Solution {
     public String reverseWords(String s) {
         if (s == null) return "";
@@ -10,5 +11,29 @@ public class Solution {
         }
         sb.append(segs[0]);
         return sb.toString();
+    }
+}
+
+// treat string as a char array
+public class Solution2 {
+    public String reverseWords(String s) {
+        if (s == null) return "";
+        List<String> words = new ArrayList<String>();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                words.add(sb.toString());
+                sb = new StringBuffer();
+            }
+            else
+                sb.append(s.charAt(i));
+        }
+        words.add(sb.toString());
+        sb = new StringBuffer();
+        for (int i = words.size() - 1; i >= 0; i--) {
+            if (!words.get(i).isEmpty())
+                sb.append(words.get(i)).append(' ');
+        }
+        return sb.toString().trim();
     }
 }
