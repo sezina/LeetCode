6af07a2eb60b53e288cbc87417d8be9443fda5d7
sezina <sezinamd@gmail.com>
2014-08-20 17:45:19 +0800

    updt: PopulatingNextRightPointersInEachNode.java

--- JAVA/PopulatingNextRightPointersInEachNode.java
+++ JAVA/PopulatingNextRightPointersInEachNode.java
@@ -28,3 +28,21 @@ public class Solution {
         }
     }
 }
+
+// real constant space
+public class Solution2 {
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        TreeLinkNode pre = root, cur = null;
+        while (pre.left != null) {
+            cur = pre;
+            while (cur != null) {
+                cur.left.next = cur.right;
+                if (cur.next != null)
+                    cur.right.next = cur.next.left;
+                cur = cur.next;
+            }
+            pre = pre.left;
+        }
+    }
+}
