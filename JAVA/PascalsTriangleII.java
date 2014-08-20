6afbe720556bbdaca81f15e0d308f5ee6a35ef54
sezina <sezinamd@gmail.com>
2014-08-20 11:08:48 +0800

    updt: PascalsTriangleII.java

--- JAVA/PascalsTriangleII.java
+++ JAVA/PascalsTriangleII.java
@@ -17,3 +17,17 @@ public class Solution {
         return Arrays.asList(result);
     }
 }
+
+public class Solution2 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<Integer>(rowIndex + 1);
+        result.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            result.add(0);
+            for (int j = i; j > 0; j--)
+                result.set(j, result.get(j) + result.get(j - 1));
+        }
+        
+        return result;
+    }
+}
