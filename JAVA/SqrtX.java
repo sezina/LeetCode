0af92184080bad163fe1895ba79e018c39bf893f
sezina <sezinamd@gmail.com>
2014-08-21 18:45:15 +0800

    updt: SqrtX.java

--- JAVA/SqrtX.java
+++ JAVA/SqrtX.java
@@ -9,3 +9,20 @@ public class Solution {
         return --i;
     }
 }
+
+// binary search
+public class Solution2 {
+    public int sqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        int low = 0, high = x, mid = 0;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (low == mid) break;
+            if (x / mid >= mid)
+                low = mid;
+            else 
+                high = mid;
+        }
+        return low;
+    }
+}
