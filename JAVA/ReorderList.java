0db3ee37b5e0a2e4ac5ef36e037b956ef9ea5337
sezina <sezinamd@gmail.com>
2014-08-24 10:42:56 +0800

    updt: ReorderList.java

--- JAVA/ReorderList.java
+++ JAVA/ReorderList.java
@@ -27,3 +27,39 @@ public class Solution {
         arr.get(n / 2).next = null;
     }
 }
+
+// O(1) space solution
+public class Solution2 {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+        ListNode slow = head, fast = head.next, temp;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (slow.next != null)
+            fast = reverse(slow.next);
+        slow.next = null;
+        slow = head;
+        while (slow != null && fast != null) {
+            temp = slow.next;
+            slow.next = fast;
+            slow = temp;
+            temp = fast.next;
+            fast.next = slow;
+            fast = temp;
+        }
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode tail = head, traversal = head.next, temp = head;
+        while (traversal != null) {
+            head = traversal;
+            tail.next = traversal.next;
+            head.next = temp;
+            temp = head;
+            traversal = tail.next;
+        }
+        return head;
+    }
+}
