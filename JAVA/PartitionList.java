84baef840a70bf64356ecb1e7d5f70a9fdac84ff
sezina <sezinamd@gmail.com>
2014-08-24 09:00:07 +0800

    updt: PartitionList.java

--- JAVA/PartitionList.java
+++ JAVA/PartitionList.java
@@ -41,3 +41,25 @@ public class Solution {
         return head2;
     }
 }
+
+// a more better solution
+public class Solution2 {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallHead = new ListNode(0);
+        ListNode bigHead = new ListNode(0);
+        ListNode smallTail = smallHead, bigTail = bigHead;
+        while (head != null) {
+            if (head.val < x) {
+                smallTail.next = head;
+                smallTail = smallTail.next;
+            } else {
+                bigTail.next = head;
+                bigTail = bigTail.next;
+            }
+            head = head.next;
+        }
+        bigTail.next = null;
+        smallTail.next = bigHead.next;
+        return smallHead.next;
+    }
+}
