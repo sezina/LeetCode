fc605e36e5f6fa4cc338c911418e0d99dd728a3b
sezina <sezinamd@gmail.com>
2014-08-30 10:51:31 +0800

    updt: SearchInRotatedSortedArray.java

--- JAVA/SearchInRotatedSortedArray.java
+++ JAVA/SearchInRotatedSortedArray.java
@@ -1,4 +1,4 @@
-// naive solution, O(n)
+// bruce solution, O(n)
 public class Solution {
     public int search(int[] A, int target) {
         for (int i = 0; i < A.length; i++)
@@ -13,22 +13,17 @@ public class Solution {
         int low = 0, high = A.length - 1, mid;
         while (low <= high) {
             mid = (low + high) / 2;
-            if (A[high] < A[low]) {
-                if (target == A[mid]) return mid;
-                else if (target == A[low]) return low;
-                else if (target == A[high]) return high;
-                else if (target > A[mid]) {
-                    if (A[low] < target && A[mid] > A[low]) low = mid + 1;
-                    else if (A[mid] < A[low] && target < A[low]) low = mid + 1;
-                    else high = mid - 1;
-                } else if (target < A[mid]) {
-                    if (A[mid] > A[low] && target < A[low]) low = mid + 1;
-                    else high = mid - 1;
-                }
+            if (A[mid] == target) return mid;
+            if (A[low] <= A[mid]) {
+                if (target < A[mid] && target >= A[low])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
             } else {
-                if (A[mid] < target) low = mid + 1;
-                else if (A[mid] > target) high = mid - 1;
-                else return mid;
+                if (target > A[mid] && target <= A[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
         return -1;
