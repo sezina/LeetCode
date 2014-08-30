aea1d053e1bdd3a9c3062c58173065514907b831
sezina <sezinamd@gmail.com>
2014-08-30 12:54:19 +0800

    updt: SearchInRotatedSortedArrayII.java add a iterative solution

--- JAVA/SearchInRotatedSortedArrayII.java
+++ JAVA/SearchInRotatedSortedArrayII.java
@@ -26,3 +26,28 @@ public class Solution {
         return false;
     }
 }
+
+// iterative solution
+public class Solution {
+    public boolean search(int[] A, int target) {
+        int mid, low = 0, high = A.length - 1;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (A[mid] == target) return true;
+            if (A[low] < A[mid]) {
+                if (target < A[mid] && target >= A[low])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else if (A[low] > A[mid]) {
+                if (target > A[mid] && target <= A[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            } else {
+                low++;
+            }
+        }
+        return false;
+    }
+}
