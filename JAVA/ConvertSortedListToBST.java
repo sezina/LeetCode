3f4046a00468a6c874fd1526fa1802222a2621b1
sezina <sezinamd@gmail.com>
2014-08-31 09:14:07 +0800

    add: ConvertSortedListToBST.java

---
+++ JAVA/ConvertSortedListToBST.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode treeHead = new TreeNode(slow.next.val);
+        treeHead.right = sortedListToBST(slow.next.next);
+        slow.next.next = null;
+        slow.next = null;
+        treeHead.left = sortedListToBST(head);
+        return treeHead;
+    }
+}
--- JAVA/README.md
+++ JAVA/README.md
@@ -1,5 +1,5 @@
 
-Solved Count: 70
+Solved Count: 71
 
 * [Linked List Cycle](https://oj.leetcode.com/problems/linked-list-cycle/)([Solution](https://github.com/sezina/LeetCode/blob/master/JAVA/LinkedListCycle.java))
 * [Single Number](https://oj.leetcode.com/problems/single-number/)
@@ -71,3 +71,4 @@ Solved Count: 70
 * [Search in Rotated Sorted Array II](https://oj.leetcode.com/problems/search-in-rotated-sorted-array-ii/)
 * [Search for a Range](https://oj.leetcode.com/problems/search-for-a-range/)
 * [Combination Sum](https://oj.leetcode.com/problems/combination-sum/)
+* [Convert Sorted List to Binary Search Tree](https://oj.leetcode.com/problems/convert-sorted-list-to-binary-search-tree/)
