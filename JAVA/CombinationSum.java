47adc43682afe910451b966f09b66f608bb0068c
sezina <sezinamd@gmail.com>
2014-08-31 08:43:27 +0800

    updt: CombinationSum.java

--- JAVA/CombinationSum.java
+++ JAVA/CombinationSum.java
@@ -26,3 +26,32 @@ public class Solution {
         }
     }
 }
+
+// another thought
+public class Solution {
+    List<List<Integer>> results;
+    List<Integer> oneRst;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        results = new LinkedList<List<Integer>>();
+        oneRst = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        if (target < candidates[0]) return results;
+        combineHelper(candidates, target, 0);
+        return results;
+    }
+    
+    private void combineHelper(int[] candidates, int target, int startIndex) {
+        for (int i = startIndex; i < candidates.length; i++) {
+            if (candidates[i] == target) {
+                oneRst.add(candidates[i]);
+                results.add(new ArrayList<Integer>(oneRst));
+                oneRst.remove(oneRst.size() - 1);
+                return;
+            } else if (candidates[i] < target) {
+                oneRst.add(candidates[i]);
+                combineHelper(candidates, target - candidates[i], i);
+                oneRst.remove(oneRst.size() - 1);
+            } else return;
+        }
+    }
+}
