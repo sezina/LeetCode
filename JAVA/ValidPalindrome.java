cc92717e015b6d540766b305e956b668b0beb92e
sezina <sezinamd@gmail.com>
2014-09-02 09:42:40 +0800

    updt: ValidPalindrome.java

--- JAVA/ValidPalindrome.java
+++ JAVA/ValidPalindrome.java
@@ -5,12 +5,8 @@ public class Solution {
         if (s.isEmpty()) return true;
         int i = 0, j = s.length() - 1;
         while (i < j) {
-            while (i < j &&
-                    !((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') ||
-                            (s.charAt(i) >= '0' && s.charAt(i) <= '9'))) i++;
-            while (i < j &&
-                    !((s.charAt(j) >= 'a' && s.charAt(j) <= 'z') ||
-                            (s.charAt(j) >= '0' && s.charAt(j) <= '9'))) j--;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
             if (i < j) {
                 if (s.charAt(i) != s.charAt(j)) return false;
                 i++;
