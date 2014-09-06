47b6963084ddea6796404296c8a122573cc6abf8
sezina <sezinamd@gmail.com>
2014-09-06 09:42:56 +0800

    updt: MinimumPathSum.java

--- JAVA/MinimumPathSum.java
+++ JAVA/MinimumPathSum.java
@@ -3,7 +3,7 @@ public class Solution {
         if (grid.length == 0 || grid[0].length == 0) return 0;
         int[][] ps = new int[grid.length][grid[0].length];
         ps[0][0] = grid[0][0];
-        int sum = 0, row = ps.length, col = ps[0].length;
+        int row = ps.length, col = ps[0].length;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (i == 0 && j != 0)
@@ -16,5 +16,25 @@ public class Solution {
             }
         }
         return ps[row - 1][col - 1];
+    }
+}
+
+// in space
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) return 0;
+        int row = grid.length, col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 && j != 0)
+                    grid[i][j] += grid[i][j - 1];
+                else if (i != 0 && j == 0)
+                    grid[i][j] += grid[i - 1][j];
+                else if (i == 0 && j == 0) continue;
+                else 
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[row - 1][col - 1];
     }
 }
