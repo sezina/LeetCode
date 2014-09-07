7e15d5b5e673341530de4ab7a25edd8b536bb9dc
sezina <sezinamd@gmail.com>
2014-09-07 16:13:17 +0800

    updt: UniquePaths.java

--- JAVA/UniquePaths.java
+++ JAVA/UniquePaths.java
@@ -2,13 +2,13 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
         if (m == 1 || n == 1) return 1;
-        int[][] dp = new int[m][n];
+        int[][] result = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) dp[i][j] = 1;
-                else dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                if (i == 0 || j == 0) result[i][j] = 1;
+                else result[i][j] = result[i][j - 1] + result[i - 1][j];
             }
         }
-        return dp[m-1][n-1];
+        return result[m-1][n-1];
     }
 }
