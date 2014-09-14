595a2eda6ecadc65b86955949cff35716c1d4ced
sezina <sezinamd@gmail.com>
2014-09-14 13:34:27 +0800

    updt: 3SumClosest.java

--- JAVA/3SumClosest.java
+++ JAVA/3SumClosest.java
@@ -23,3 +23,27 @@ public class Solution {
         }
     }
 }
+
+// iterative solution
+public class Solution {
+    public int threeSumClosest(int[] num, int target) {
+        Arrays.sort(num);
+        int max = num[0] + num[1] + num[2];
+        if (num.length == 3) return max;
+        int n = num.length;
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = num[i] + num[j] + num[k];
+                if (Math.abs(target - sum) < Math.abs(target - max)) {
+                    max = sum;
+                    if (max == target) return max;
+                }
+                if (sum > target) k--;
+                else j++;
+            }
+        }
+        return max;
+    }
+}
