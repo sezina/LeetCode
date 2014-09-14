0811fe3d53e0f569f747f883b89626543c56babb
sezina <sezinamd@gmail.com>
2014-09-14 14:03:26 +0800

    updt: BestTimeToBuyAndSellStockIII.java

--- JAVA/BestTimeToBuyAndSellStockIII.java
+++ JAVA/BestTimeToBuyAndSellStockIII.java
@@ -24,3 +24,29 @@ public class Solution {
         return max;
     }
 }
+
+// O(n)
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2) return 0;
+        int max = 0, sum = 0, maxPrice = prices[prices.length - 1];
+        int[] preProfit = new int[prices.length];
+        int[] backProfit = new int[prices.length];
+        
+        // forward
+        for (int i = 1; i < prices.length; i++) {
+            sum += (prices[i] - prices[i - 1]);
+            preProfit[i] = Math.max(sum, preProfit[i - 1]);
+            if (sum < 0) sum = 0;
+        }
+        
+        // backward
+        max = preProfit[prices.length - 1] + backProfit[prices.length - 1];
+        for (int i = prices.length - 2; i >= 0; i--) {
+            maxPrice = Math.max(maxPrice, prices[i]);
+            backProfit[i] = Math.max(backProfit[i + 1], maxPrice - prices[i]);
+            max = Math.max(max, backProfit[i] + preProfit[i]);
+        }
+        return max;
+    }
+}
