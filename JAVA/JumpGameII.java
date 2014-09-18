dfd5833d68787ffc23c3ea9fa45f4d31e63f49ad
sezina <sezinamd@gmail.com>
2014-09-19 06:48:38 +0800

    updt: JumpGameII.java

--- JAVA/JumpGameII.java
+++ JAVA/JumpGameII.java
@@ -1,3 +1,4 @@
+// O(n^2) time
 public class Solution {
     public int jump(int[] A) {
         if (A.length < 2) return 0;
@@ -15,7 +16,7 @@ public class Solution {
     private int jumpHelper(int[] A, int target) {
         int step = A[0];
         if (step >= target) return 0;
-        for (int i = 0; step > 0; i++) {
+        for (int i = 1; step > 0; i++) {
             if (i + step >= target) {
                 target = i;
                 break;
@@ -23,5 +24,28 @@ public class Solution {
             step = Math.max(step - 1, A[i]);
         }
         return target;
+    }
+}
+
+// O(n) time and O(n) space
+public class Solution {
+    public int jump(int[] A) {
+        int n = A.length;
+        if (n == 1) return 0;
+        int[] res = new int[n];
+        int step = A[0], index = Math.min(A[0], n - 1);
+        if (index == n - 1) return 1;
+        res[index] = 1;
+        for (int i = 1; step > 0; i++) {
+            if (i < index)
+                res[i] = res[index];
+            step = Math.max(step - 1, A[i]);
+            if (i == index) {
+                index = Math.min(i + step, n - 1);
+                res[index] = res[i] + 1;
+                if (index == n - 1) break;
+            }
+        }
+        return res[n - 1];
     }
 }
