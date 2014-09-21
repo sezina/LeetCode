6a86669f27c88bdfbd2141b4f4baeb7ef4ca78ca
sezina <sezinamd@gmail.com>
2014-09-21 09:08:01 +0800

    updt: LongestConsecutiveSequence.java

--- JAVA/LongestConsecutiveSequence.java
+++ JAVA/LongestConsecutiveSequence.java
@@ -20,3 +20,27 @@ public class Solution {
         return maxLen;
     }
 }
+
+// one pass loop
+public class Solution {
+    public int longestConsecutive(int[] num) {
+        if (num.length == 0) return 0;
+        if (num.length == 1) return 1;
+        Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
+        int maxLen = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (countMap.containsKey(num[i])) continue;
+            int low = num[i], up = num[i];
+            if (countMap.containsKey(num[i] - 1))
+                low = countMap.get(num[i] - 1);
+            if (countMap.containsKey(num[i] + 1))
+                up = countMap.get(num[i] + 1);
+            
+            maxLen = Math.max(maxLen, up - low + 1);
+            countMap.put(num[i], num[i]);
+            countMap.put(low, up);
+            countMap.put(up, low);
+        }
+        return maxLen;
+    }
+}
