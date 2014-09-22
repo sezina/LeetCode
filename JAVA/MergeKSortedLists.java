e4648c6c6964505dce6e707e895d96bc2c3e8aab
sezina <sezinamd@gmail.com>
2014-09-22 10:37:56 +0800

    updt: MergeKSortedLists.java

--- JAVA/MergeKSortedLists.java
+++ JAVA/MergeKSortedLists.java
@@ -40,3 +40,29 @@ public class Solution {
         return map.get(vals.get(0));
     }
 }
+
+// priority queue
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists.isEmpty()) return null;
+        if (lists.size() == 1) return lists.get(0);
+        ListNode fakeHead = new ListNode(0);
+        ListNode temp = fakeHead;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>() {
+            public int compare(ListNode n1, ListNode n2) {
+                if (n1.val < n2.val) return -1;
+                else if (n1.val > n2.val) return 1;
+                return 0;
+            }
+        });
+        for (int i = 0; i < lists.size(); i++)
+            if (lists.get(i) != null) queue.add(lists.get(i));
+        
+        while (!queue.isEmpty()) {
+            temp.next = queue.poll();
+            temp = temp.next;
+            if (temp.next != null) queue.add(temp.next);
+        }
+        return fakeHead.next;
+    }
+}
