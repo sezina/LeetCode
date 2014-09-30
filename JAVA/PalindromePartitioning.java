c0d18aec8dc3420725d67356b9c27abb4313e37a
sezina <sezinamd@gmail.com>
2014-09-30 10:54:58 +0800

    updt: PalindromePartitioning.java

--- JAVA/PalindromePartitioning.java
+++ JAVA/PalindromePartitioning.java
@@ -1,3 +1,4 @@
+// split all palindrome substring and dfs
 public class Solution {
     List<List<String>> res;
     public List<List<String>> partition(String s) {
@@ -42,6 +43,47 @@ public class Solution {
             ans.add(temp);
             partitionHelper(s, map, ans, index + temp.length());
             ans.pop();
+        }
+    }
+}
+
+// dynamic programming and dfs
+public class Solution {
+    List<List<String>> res;
+    public List<List<String>> partition(String s) {
+        res = new ArrayList<List<String>>();
+        if (s.isEmpty()) return res;
+        
+        int len = s.length();
+        boolean[][] isPalin = new boolean[len][len];
+        
+        for (int i = 0; i < len; i++)
+            isPalin[i][i] = true;
+        
+        for (int j = 0; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (j == i + 1) isPalin[i][j] = (s.charAt(i) == s.charAt(j));
+                else isPalin[i][j] = (s.charAt(i) == s.charAt(j) && isPalin[i + 1][j - 1]);
+            }
+        }
+        
+        partitionHelper(s, isPalin, new Stack<String>(), 0);
+        return res;
+    }
+    
+    private void partitionHelper(String s, boolean[][] isPalin, Stack<String> stack, int index) {
+        if (index == s.length()) {
+            res.add(new ArrayList<String>(stack));
+            return;
+        }
+        
+        for (int i = index; i < s.length(); i++) {
+            if (isPalin[index][i]) {
+                String subs = s.substring(index, i + 1);
+                stack.add(subs);
+                partitionHelper(s, isPalin, stack, i + 1);
+                stack.pop();
+            }
         }
     }
 }
