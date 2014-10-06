93ba6827bd38fcddd5ea5a43b16c8fe8fc42297e
sezina <sezinamd@gmail.com>
2014-10-06 21:28:27 +0800

    updt: SetMatrixZeros.java

--- JAVA/SetMatrixZeroes.java
+++ JAVA/SetMatrixZeroes.java
@@ -19,3 +19,42 @@ public class Solution {
                 matrix[i][c] = 0;
     }
 }
+
+// O(1) space
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean rz = false, cz = false;
+        for (int i = 0; i < matrix[0].length; i++)
+            if (matrix[0][i] == 0) rz = true;
+        for (int i = 0; i < matrix.length; i++)
+            if (matrix[i][0] == 0) cz = true;
+        
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (cz && i == 0) continue;
+            if (matrix[0][i] == 0) {
+                for (int j = 0; j < matrix.length; j++)
+                    matrix[j][i] = 0;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < matrix[0].length; j++)
+                    matrix[i][j] = 0;
+            }
+        }
+        if (rz)
+            for (int i = 0; i < matrix[0].length; i++) matrix[0][i] = 0;
+        if (cz)
+            for (int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+    }
+}
