81f1cdbc718fe8f8cb26913b6dfec7d98b6ff5a8
sezina <sezinamd@gmail.com>
2014-10-21 10:03:42 +0800

    updt: SurroundedRegions.java

--- JAVA/SurroundedRegions.java
+++ JAVA/SurroundedRegions.java
@@ -45,3 +45,55 @@ public class Solution {
     }
 
 }
+
+// bfs
+public class Solution {
+    private static int[] roffset = {0,-1,1,0};
+    private static int[] coffset = {-1,0,0,1};
+    public void solve(char[][] board) {
+        if (board.length <= 2 || board[0].length <= 2) return;
+        int rows = board.length, cols = board[0].length;
+        boolean[][] flag = new boolean[rows][cols];
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        
+        for (int i = 0; i < cols - 1; i++) {
+            if (board[0][i] == 'O') {
+                queue.add(new int[]{0, i});
+                flag[0][i] = true;
+            }
+            if (board[rows - 1][cols - i - 1] == 'O') {
+                queue.add(new int[]{rows - 1, cols-i-1});
+                flag[rows - 1][cols-i-1] = true;
+            }
+        }
+        
+        for (int i = 1; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                queue.add(new int[]{i, 0});
+                flag[i][0] = true;
+            }
+            if (board[rows - i - 1][cols - 1] == 'O') {
+                queue.add(new int[]{rows-i-1, cols-1});
+                flag[rows-i-1][cols-1] = true;
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            int[] cor = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nr = cor[0] + roffset[i], nc = cor[1] + coffset[i];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                    if (board[nr][nc] == 'O' && flag[nr][nc] == false) {
+                        queue.add(new int[]{nr, nc});
+                        flag[nr][nc] = true;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                if (flag[i][j] == false && board[i][j] == 'O')
+                    board[i][j] = 'X';
+    }
+}
