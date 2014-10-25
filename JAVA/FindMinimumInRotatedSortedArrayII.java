9e2e5176ddb5c960325aaa1e80898bf41b9d2490
sezina <sezinamd@gmail.com>
2014-10-25 08:02:29 +0800

    updt: FindMinimumInRotatedSortedArrayII.java

--- JAVA/FindMinimumInRotatedSortedArrayII.java
+++ JAVA/FindMinimumInRotatedSortedArrayII.java
@@ -8,27 +8,47 @@ public class Solution {
     }
 }
 
-// binary search, worse case O(n)
+// binary search
 public class Solution {
     public int findMin(int[] num) {
         int low = 0, high = num.length - 1;
-        while (low + 1 < high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if (num[mid] > num[low]) {
-                if (num[mid] < num[high]) return num[low];
-                else if (num[mid] == num[high]) high = mid;
-                else low = mid;
-            } else if (num[mid] < num[low]) {
-                high = mid;
+            if (num[high] > num[low]) return num[low];
+            else if (num[high] < num[low]) {
+                if (num[mid] < num[low]) high = mid;
+                else low = mid + 1;
             } else {
-                if (num[mid] < num[high]) return num[low];
-                else if (num[mid] > num[high]) low = mid;
+                if (num[mid] < num[low]) high = mid;
+                else if (num[mid] > num[low]) low = mid + 1;
                 else {
                     low++;
                     high--;
                 }
             }
         }
-        return Math.min(num[low], num[high]);
+        return num[low];
+    }
+}
+
+// more neat code
+public class Solution {
+    public int findMin(int[] num) {
+        int low = 0, high = num.length - 1;
+        while (low < high) {
+            if (num[high] > num[low]) return num[low];
+            
+            int mid = low + (high - low) / 2;
+            if (num[mid] < num[low]) high = mid;
+            else if (num[mid] > num[low]) low = mid + 1;
+            else {
+                if (num[high] < num[low]) low = mid + 1;
+                else {
+                    low++;
+                    high--;
+                }
+            }
+        }
+        return num[low];
     }
 }
