0a2a5666678a0625f7b93cf0168cb42a03c290f7
sezina <sezinamd@gmail.com>
2014-11-14 17:11:33 +0800

    updt: PlusOne.java

--- JAVA/PlusOne.java
+++ JAVA/PlusOne.java
@@ -1,27 +1,15 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
-        boolean carry = false;
-        int n = digits.length;
-        digits[n - 1] += 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (carry)
-                digits[i] += 1;
-            if (digits[i] > 9) {
-                carry = true;
-                digits[i] -= 10;
-            } else
-                carry = false;
+        digits[digits.length - 1] += 1;
+        for (int i = digits.length - 1; i > 0; i--) {
+            digits[i - 1] += (digits[i] / 10);
+            digits[i] %= 10;
         }
-        int[] result;
-        if (carry) {
-            result = new int[n + 1];
-            result[0] = 1;
-            for (int i = 0; i < n; i++)
-                result[i + 1] = digits[i];
-            return result;
-        } else {
-            result = digits;
-        }
-        return result;
+        if (digits[0] < 10) return digits;
+        digits[0] %= 10;
+        int[] res = new int[digits.length + 1];
+        System.arraycopy(digits, 0, res, 1, digits.length);
+        res[0] = 1;
+        return res;
     }
 }
