20c096469b558a839a651cc28ca02715198d7f07
sezina <sezinamd@gmail.com>
2014-11-17 14:51:38 +0800

    updt: ImplementStrStr.java

--- JAVA/ImplementStrStr.java
+++ JAVA/ImplementStrStr.java
@@ -1,22 +1,14 @@
 public class Solution {
-    public String strStr(String haystack, String needle) {
-        if (haystack == null || needle == null ||
-            needle.length() > haystack.length())
-            return null;
-        if (needle.isEmpty()) return haystack;
-        boolean isFind = true;
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() < needle.length())
+            return -1;
+        if (needle.isEmpty()) return 0;
         for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            if (haystack.charAt(i) != needle.charAt(0))
-                continue;
-            isFind = true;
-            for (int j = 1; j < needle.length(); j++) {
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    isFind = false;
-                    break;
-                }
-            }
-            if (isFind) return haystack.substring(i);
+            if (haystack.charAt(i) != needle.charAt(0)) continue;
+            int j = 1;
+            for (; j < needle.length() && haystack.charAt(i+j) == needle.charAt(j); j++);
+            if (j == needle.length()) return i;
         }
-        return null;
+        return -1;
     }
 }
